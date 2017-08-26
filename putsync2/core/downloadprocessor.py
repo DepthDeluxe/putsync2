4d90bde8e695d5a61dd48220af0d4064c5e14ff1
DepthDeluxe <colin@heinzmann.me>
2017-08-26 08:23:32 -0700

    cleaned up download codebase by removing go() function

--- putsync2/core/downloadprocessor.py
+++ putsync2/core/downloadprocessor.py
@@ -17,7 +17,29 @@ def processdownload(id):
     if not checkdownloadvalidandmarkinprogress(id):
         return
 
-    go(id)
+    # get the remote file and disable file verification because it is slow
+    remote_file = putiopy.Client(getputsyncconfig().putio_token).File.get(id)
+    __disable_file_verification(remote_file)
+
+    logger.info(f'Starting download of {remote_file.name}')
+
+    with db_session:
+        full_local_media_path = os.path.dirname(os.path.join(getputsyncconfig().media_path, Download[id].filepath))
+        try:
+            os.makedirs(full_local_media_path)
+        except FileExistsError:
+            logger.info(f'Folder {full_local_media_path} already exists')
+            pass
+
+    if getputsyncconfig().disable_downloading:
+        logger.warn(f'Not actually downloading {remote_file.name}, configured to disable real downloading')
+    else:
+        remote_file.download(dest=full_local_media_path)
+   
+    # commit the results
+    with db_session:
+        Download[id].markdone()
+    logger.info(f'{remote_file.name} download successful')
 
 
 @locked
@@ -26,7 +48,7 @@ def checkdownloadvalidandmarkinprogress(id):
     download = Download[id]
 
     if download.status != DownloadStatus.new.value:
-        logger.warn(f'Attempting to download {download.remote_file_id} when in {download.status} state, skipping')
+        logger.debug(f'Attempting to download {download.remote_file_id} when in {download.status} state, skipping')
         return False
     else:
         download.markinprogress()
@@ -39,32 +61,6 @@ def __disable_file_verification(remote_file):
 
     remote_file._verify_file = __new_verify_file
     logger.info('File verification disabled')
-
-
-@db_session
-def go(id):
-    download = Download[id]
-
-    # get the remote file and disable file verification because it is slow
-    remote_file = putiopy.Client(getputsyncconfig().putio_token).File.get(id)
-    __disable_file_verification(remote_file)
-
-    logger.info(f'Starting download of {remote_file.name}')
-
-    full_local_media_path = os.path.dirname(os.path.join(getputsyncconfig().media_path, download.filepath))
-    try:
-        os.makedirs(full_local_media_path)
-    except FileExistsError:
-        logger.info(f'Folder {full_local_media_path} already exists')
-        pass
-
-    if getputsyncconfig().disable_downloading:
-        logger.warn(f'Not actually downloading {remote_file.name}, configured to disable real downloading')
-    else:
-        remote_file.download(dest=full_local_media_path)
-
-    download.markdone()
-    logger.info(f'{remote_file.name} download successful')
 
 
 @db_session
